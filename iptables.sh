c9f1f1ce0e7116808a71f74c6d57247a57e06c6f
班文政 <1204470409@qq.com>
2025-03-26 09:40:21 +0800

    添加 iptables 脚本地址和下载链接

--- iptables.sh
+++ iptables.sh
@@ -1,3 +1,18 @@
+脚本地址：https://doc.sensorsdata.cn/display/~huayang/change_iptables.sh
+
+# 如果环境有外网，会自动下载新版 iptables 的 rpm 包，如果环境无外网，请下载上传 iptables 的 rpm 包到 /home/sa_cluster 目录下
+  
+wget http://update.sensorsdata.cn/specialfile/iptables-services-1.4.21-35.el7.x86_64.rpm
+MD5：b7ce9505d0bc06fb7efd43ff70778662        
+ 
+wget http://update.sensorsdata.cn/specialfile/iptables-1.4.21-35.el7.x86_64.rpm
+MD5：e3001c23d5039664c09ecdd64b08831b
+
+# sftp 固定连接 
+wget http://update.sensorsdata.cn/specialfile/change_iptables.sh
+
+
+
 sudo cp /etc/sysconfig/iptables /home/sa_cluster/iptables.bak-20250303
 sudo cat -n /etc/sysconfig/iptables
 
